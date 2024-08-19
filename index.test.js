2e2e5b85b95f434ced85cbd288f65275521ba86c
piyush srivastava <piyush.x.srivastava.-nd@disney.com>
2024-08-19 22:55:19 +0530

    Initial commit

---
+++ index.test.js
@@ -0,0 +1,16 @@
+// index.test.js
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+app.get('/', (req, res) => {
+  res.status(200).send('Hello World!');
+});
+
+describe('GET /', () => {
+  it('responds with Hello World!', (done) => {
+    request(app)
+      .get('/')
+      .expect('Hello World!', done);
+  });
+});
